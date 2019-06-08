1f87099a09983a5eea73d420bdeee64909e6312d
ingvillajimenez <ing.villa.jimenez@gmail.com>
2019-06-08 10:18:22 -0500

    boilerplate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
+
+// npx create-react-app react-todo
+// npm run
+// npm run eject (habilita todo lo de la configuracion)
+// manifest.json en public es parte de las progresive web apps
+// serviceworker va ligado al manifest es para las progresive web apps
